36db3349bd7a9d8848e354eb74f1655c31df017b
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-03-29 21:30:18 -0400

    cola/issue #1: users can perform crud ops on user model

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,90 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// get all users (/api/users)
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password']}
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+// get user by id (/api/users/id)
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id!'})
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+// post a new user (/api/users)
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    password: req.body.password
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+// update a user by id (/api/users/id)
+router.put('/:id', (req, res) => {
+  User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with that id!'});
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+// delete a user by id (/api/users/id)
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with that id!'});
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+});
+
+module.exports = router;
